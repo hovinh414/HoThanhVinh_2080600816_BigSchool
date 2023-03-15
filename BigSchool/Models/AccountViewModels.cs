0d510a7c37e8018eccf33d6319dcb33b40d33984
hovinh414 <hovinh414@gmail.com>
2023-03-15 10:56:37 +0700

    update buoi 2

--- BigSchool/Models/AccountViewModels.cs
+++ BigSchool/Models/AccountViewModels.cs
@@ -68,7 +68,9 @@ namespace BigSchool.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
-
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
@@ -78,6 +80,7 @@ namespace BigSchool.Models
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        
         public string ConfirmPassword { get; set; }
     }
 
